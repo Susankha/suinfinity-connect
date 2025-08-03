0f309e7444b5db3ef80e63b546045e12a375a0e9
susankha <susankhanirmala@gmail.com>
2025-08-03 20:22:33 +0530

    added suinfinity project

---
+++ suinfinity/src/main/java/org/susa/suinfinity/SuinfinityApplication.java
@@ -0,0 +1,13 @@
+package org.susa.suinfinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuinfinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuinfinityApplication.class, args);
+	}
+
+}
---
+++ suinfinity/src/test/java/org/susa/suinfinity/SuinfinityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.susa.suinfinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuinfinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
