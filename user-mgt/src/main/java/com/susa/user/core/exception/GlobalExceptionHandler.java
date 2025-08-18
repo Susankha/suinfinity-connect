daa347ef994a70ef823de20a8539bf273d70ac26
susankha <susankhanirmala@gmail.com>
2025-08-18 21:56:48 +0530

    changed arg validation error log

--- user-mgt/src/main/java/com/susa/user/core/exception/GlobalExceptionHandler.java
+++ user-mgt/src/main/java/com/susa/user/core/exception/GlobalExceptionHandler.java
@@ -32,7 +32,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             .map(fieldError -> fieldError.getDefaultMessage())
             .toList();
     responseBody.put("errors", errors);
-    logger.error("Validation failed for argument ", ex);
+    logger.error("Validation failed : ", ex);
 
     return new ResponseEntity<>(responseBody, headers, status);
   }
