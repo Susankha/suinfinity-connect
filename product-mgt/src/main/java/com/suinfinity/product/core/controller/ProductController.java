943b8ff1bc9c279cccd697e737fb77f330eae634
susankha <susankhanirmala@gmail.com>
2025-09-04 13:24:40 +0530

    product updating and deleting added

--- product-mgt/src/main/java/com/suinfinity/product/core/controller/ProductController.java
+++ product-mgt/src/main/java/com/suinfinity/product/core/controller/ProductController.java
@@ -5,12 +5,15 @@ import com.suinfinity.product.core.dto.ProductResponseDTO;
 import com.suinfinity.product.core.service.ProductService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +32,21 @@ public class ProductController {
   @GetMapping(value = "/get/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
   private ResponseEntity<ProductResponseDTO> getProduct(@NotBlank @PathVariable String name) {
     return productService.getProduct(name);
+  }
+
+  @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+  private ResponseEntity<List<ProductResponseDTO>> getProducts() {
+    return productService.getProducts();
+  }
+
+  @PutMapping(value = "/update/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+  private ResponseEntity<ProductResponseDTO> updateProduct(
+      @NotBlank @PathVariable String name, @Valid @RequestBody ProductDTO productDTO) {
+    return productService.updateProduct(name, productDTO);
+  }
+
+  @DeleteMapping(value = "/delete/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+  private ResponseEntity<?> deleteProduct(@NotBlank @PathVariable String name) {
+    return productService.deleteProduct(name);
   }
 }
--- product-mgt/src/main/java/com/suinfinity/product/core/service/ProductService.java
+++ product-mgt/src/main/java/com/suinfinity/product/core/service/ProductService.java
@@ -5,11 +5,13 @@ import com.suinfinity.product.core.dto.ProductResponseDTO;
 import com.suinfinity.product.core.mapper.ProductMapper;
 import com.suinfinity.product.core.model.Product;
 import com.suinfinity.product.core.repository.ProductRepository;
+import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Log4j2
 @Service
@@ -35,5 +37,33 @@ public class ProductService {
     ProductResponseDTO productResponseDTO = ProductMapper.INSTANCE.toProductResponseDTO(product);
 
     return ResponseEntity.ok().body(productResponseDTO);
+  }
+
+  public ResponseEntity<List<ProductResponseDTO>> getProducts() {
+    List<Product> products = productRepository.findAll();
+    List<ProductResponseDTO> productResponseDTOS =
+        products.stream().map(ProductMapper.INSTANCE::toProductResponseDTO).toList();
+
+    return ResponseEntity.ok().body(productResponseDTOS);
+  }
+
+  public ResponseEntity<ProductResponseDTO> updateProduct(
+      String productName, ProductDTO productDTO) {
+    Product product = productRepository.findByName(productName);
+    product.setName(productDTO.getName());
+    product.setDescription(productDTO.getDescription());
+    product.setPrice(productDTO.getPrice());
+    product.setStockQuantity(productDTO.getStockQuantity());
+
+    productRepository.save(product);
+    ProductResponseDTO productResponseDTO = ProductMapper.INSTANCE.toProductResponseDTO(product);
+
+    return ResponseEntity.ok().body(productResponseDTO);
+  }
+
+  @Transactional
+  public ResponseEntity<Void> deleteProduct(String name) {
+    productRepository.deleteByName(name);
+    return ResponseEntity.noContent().build();
   }
 }
