1ea43b0001a3a8a2452ecbd9efc2ba4c64e417c1
susankha <susankhanirmala@gmail.com>
2025-09-04 22:25:30 +0530

    handle empty results from reprository

--- product-mgt/src/main/java/com/suinfinity/product/core/repository/ProductRepository.java
+++ product-mgt/src/main/java/com/suinfinity/product/core/repository/ProductRepository.java
@@ -1,13 +1,14 @@
 package com.suinfinity.product.core.repository;
 
 import com.suinfinity.product.core.model.Product;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-  Product findByName(String name);
+  Optional<Product> findByName(String name);
 
   long deleteByName(String name);
 }
