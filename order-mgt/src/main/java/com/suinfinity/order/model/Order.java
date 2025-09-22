af12e0e3cce9fafdfd4e78b94b049eed96db472c
susankha <susankhanirmala@gmail.com>
2025-09-22 10:54:11 +0530

    adding user_id to order mgt with repository

--- order-mgt/src/main/java/com/suinfinity/order/model/Order.java
+++ order-mgt/src/main/java/com/suinfinity/order/model/Order.java
@@ -1,15 +1,10 @@
 package com.suinfinity.order.model;
 
-//import com.suinfinity.user.model.User;
-import com.suinfinity.user.model.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -28,14 +23,11 @@ public class Order {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long orderId;
 
-  //fetch = FetchType.EAGER, cascade = CascadeType.ALL,
-//  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//  @JoinColumn(name = "user_id", nullable = false)
-//  private User user;
-
   @Temporal(TemporalType.TIMESTAMP)
   private Date orderDate;
 
   @Column(scale = 2)
   private BigDecimal amount;
+
+  private long user_id;
 }
