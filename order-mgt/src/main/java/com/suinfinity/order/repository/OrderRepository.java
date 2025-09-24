781d9fbf280e0089b7d9af0f52fbc8d419623292
susankha <susankhanirmala@gmail.com>
2025-09-24 18:41:01 +0530

    added curd for order-mgt

--- order-mgt/src/main/java/com/suinfinity/order/repository/OrderRepository.java
+++ order-mgt/src/main/java/com/suinfinity/order/repository/OrderRepository.java
@@ -1,9 +1,10 @@
 package com.suinfinity.order.repository;
 
 import com.suinfinity.order.model.Order;
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
