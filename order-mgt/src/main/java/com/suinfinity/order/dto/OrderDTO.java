62d446ef5fdd7724dadb5ea006793831d84bf7b7
susankha <susankhanirmala@gmail.com>
2025-10-01 16:48:28 +0530

    removed unused imports

--- order-mgt/src/main/java/com/suinfinity/order/dto/OrderDTO.java
+++ order-mgt/src/main/java/com/suinfinity/order/dto/OrderDTO.java
@@ -8,7 +8,6 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
