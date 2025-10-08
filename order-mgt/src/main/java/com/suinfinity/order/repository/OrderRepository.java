f2b531c24ae2bfe9ef32ae6c7a009210a31c80aa
susankha <susankhanirmala@gmail.com>
2025-10-08 11:59:33 +0530

    operations with wrong order id handled

--- order-mgt/src/main/java/com/suinfinity/order/repository/OrderRepository.java
+++ order-mgt/src/main/java/com/suinfinity/order/repository/OrderRepository.java
@@ -1,10 +1,11 @@
 package com.suinfinity.order.repository;
 
+import com.suinfinity.order.exception.OrderNotFoundException;
 import com.suinfinity.order.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-  Long deleteByOrderId(Long orderId);
+  void deleteByOrderId(Long orderId) throws OrderNotFoundException;
 }
