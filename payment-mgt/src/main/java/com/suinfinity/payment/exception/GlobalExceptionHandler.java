c8f7af4f0c4df8384f4d7f168463b997210d6c26
susankha <susankhanirmala@gmail.com>
2025-10-24 17:50:32 +0530

    exception handler added to payment-mgt

--- payment-mgt/src/main/java/com/suinfinity/payment/exception/GlobalExceptionHandler.java
+++ payment-mgt/src/main/java/com/suinfinity/payment/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.suinfinity.user.exception;
+package com.suinfinity.payment.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -9,13 +9,12 @@ import java.util.List;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -23,27 +22,9 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-@Slf4j
+@Log4j2
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-  @Override
-  protected ResponseEntity<Object> handleMethodArgumentNotValid(
-      MethodArgumentNotValidException ex,
-      HttpHeaders headers,
-      HttpStatusCode status,
-      WebRequest request) {
-
-    List<String> errors =
-        ex.getBindingResult().getFieldErrors().stream()
-            .map(fieldError -> fieldError.getDefaultMessage())
-            .toList();
-    String errorMessage = "Validation failed";
-    ErrorResponse errorResponse = getErrorResponse(status.value(), errorMessage, request);
-    errorResponse.setErrors(errors);
-    log.error(errorMessage, ex);
-    return ResponseEntity.status(status.value()).headers(headers).body(errorResponse);
-  }
 
   @Override
   protected ResponseEntity<Object> handleNoResourceFoundException(
@@ -54,9 +35,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     return ResponseEntity.status(status.value()).headers(headers).body(errorResponse);
   }
 
-  @ExceptionHandler(UserNotFoundException.class)
-  protected ResponseEntity<Object> handleUserNotFoundException(
-      UserNotFoundException ex, WebRequest request) {
+  @ExceptionHandler(PaymentNotFoundException.class)
+  protected ResponseEntity<Object> handlePaymentNotFoundException(
+      PaymentNotFoundException ex, WebRequest request) {
     String errorMessage = ex.getMessage();
     ErrorResponse errorResponse =
         getErrorResponse(HttpStatus.NOT_FOUND.value(), errorMessage, request);
--- payment-mgt/src/main/java/com/suinfinity/payment/repository/PaymentRepository.java
+++ payment-mgt/src/main/java/com/suinfinity/payment/repository/PaymentRepository.java
@@ -1,8 +1,9 @@
 package com.suinfinity.payment.repository;
 
+import com.suinfinity.payment.exception.PaymentNotFoundException;
 import com.suinfinity.payment.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<Payment,Long> {
-
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+  void deleteByPaymentId(Long orderId) throws PaymentNotFoundException;
 }
