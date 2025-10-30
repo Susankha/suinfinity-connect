dfcd3e4438c788afbede8cbab02e2d78af0b91f2
susankha <susankhanirmala@gmail.com>
2025-10-30 22:01:59 +0530

    get order status operation

--- order-mgt/src/main/java/com/suinfinity/order/repository/OrderRepository.java
+++ order-mgt/src/main/java/com/suinfinity/order/repository/OrderRepository.java
@@ -2,10 +2,15 @@ package com.suinfinity.order.repository;
 
 import com.suinfinity.order.exception.OrderNotFoundException;
 import com.suinfinity.order.model.Order;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
   void deleteByOrderId(Long orderId) throws OrderNotFoundException;
+
+  @Query("SELECT o.status FROM Order o WHERE o.orderId = :orderId")
+  Optional<String> findOrderStatusByOrderId(Long orderId);
 }
