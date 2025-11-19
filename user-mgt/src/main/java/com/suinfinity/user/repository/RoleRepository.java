407d5ef8683d87930c3627c560a0aef4c2d08970
susankha <susankhanirmala@gmail.com>
2025-11-19 11:55:02 +0530

    update user role with user create

--- user-mgt/src/main/java/com/suinfinity/user/repository/RoleRepository.java
+++ user-mgt/src/main/java/com/suinfinity/user/repository/RoleRepository.java
@@ -1,6 +1,12 @@
 package com.suinfinity.user.repository;
 
 import com.suinfinity.user.model.Role;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {}
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+  Optional<Role> findById(Long id);
+}
